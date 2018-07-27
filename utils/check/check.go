8d95e85ee119f1713968d2ad62bf62fd79e837b0
Gogs <gogs@fake.local>
2018-07-27 18:36:54 +0800

    as

--- utils/check/check.go
+++
@@ -1,9 +0,0 @@
-package check
-
-import "regexp"
-
-func CheckIsMobile(mobileNum string) bool {
-	var  regular = "^1[345789]{1}\\d{9}$"
-	reg := regexp.MustCompile(regular)
-	return reg.MatchString(mobileNum)
-}
